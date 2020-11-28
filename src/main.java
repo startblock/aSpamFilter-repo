78cfe741f31e410422c56679f16eabe92d7850d5
startblock <burkenigel1995@gmail.com>
2020-11-28 01:48:05 +0000

    created a main class to initialise the training data class

--- src/main.java
+++ src/main.java
@@ -1,5 +1,22 @@
 public class main {
 
+    public static void main(String[] args) {
+        Data spam=new Data();
+        Data ham=new Data();
+        fileParser.directoryParser("enron1/spam/",spam);
+        fileParser.directoryParser("enron1/ham/",ham);
+
+        spam.finalScores(ham.getTrainingWords());
+        ham.finalScores(spam.getTrainingWords());
+        spam.scoreMap(spam.getTrainingWords(),spam.getTotalWords());
+        ham.scoreMap(ham.getTrainingWords(),ham.getTotalWords());
+        spam.createFile(spam.getWordScores(),"spamScores");
+        ham.createFile(ham.getWordScores(),"hamScores");
+
+
+
+    }
+
 
 
 
